750c9dfcb9e4377856f9af3bb7d798acb973bc88
Dong-HaTran <57392353+Dong-HaTran@users.noreply.github.com>
2019-11-23 17:29:04 +0800

    First upload

---
+++ js/common.js
@@ -0,0 +1,25 @@
+$( document ).ready(function() {
+    $.get('menu.html', function(data) {
+        $($('body').find('menu')).html(data)
+        }, 'text');
+    //get content
+    var template = getUrlParameter('page') + '.html';
+    $.get(template, function(data) {
+        $($('body').find('content')).html(data)
+        }, 'text');
+    });
+
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = window.location.search.substring(1),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+    
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+    
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            }
+        }
+    };
